e9a8cd511855ed578f9c118df1fa4d6aeaac45ca
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2025-09-02 16:04:10 +0700

    feat(Navbar): add WhatsApp contact button functionality

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,14 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
 import CustomButton from "./CustomButton";
 
 const Navbar = () => {
+  const handleClick = () => {
+    window.open("https://wa.me/628112340753", "_blank");
+  };
+
   return (
     <header className="w-full absolute z-10">
       <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
@@ -20,6 +26,7 @@ const Navbar = () => {
           title="Kontak"
           btnType="button"
           containerStyles="text-primary-red rounded-full bg-white min-w-[130px] border border-primary-red"
+          handleClick={handleClick}
         />
       </nav>
     </header>
