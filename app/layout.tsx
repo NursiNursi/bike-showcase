7594dee03d21bcddb87ad1debc86ee425aa77664
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2025-09-26 16:08:05 +0700

    docs(layout): update page metadata for better SEO and clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,10 @@ import "./globals.css";
 import { Footer, Navbar } from "@/components";
 
 export const metadata: Metadata = {
-  title: "Honda Lembang",
-  description: "Dealer Honda Lembang",
+  title:
+    "Dealer Resmi Motor Honda Lembang Bandung | Cicilan Ringan & Promo Terbaru",
+  description:
+    "Dealer resmi motor Honda di Lembang Bandung ✔️ Cicilan ringan, promo motor Honda terbaru, layanan cepat & garansi resmi. Cek harga & ajukan kredit sekarang!",
 };
 
 export default function RootLayout({
