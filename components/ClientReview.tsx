38e606cfc98c4d0909033dc83552be1203845fe4
muhamadnursiramdan <muhamadnursiramdan@gmail.com>
2025-09-28 16:33:53 +0700

    refactor(ClientReview): remove flex-between and uncomment heading section

--- components/ClientReview.tsx
+++ components/ClientReview.tsx
@@ -8,11 +8,11 @@ import { allReviews } from "@/constants/reviewData";
 const ClientReview = () => {
   return (
     <div className="mt-12 padding-x padding-y max-width" id="client-review">
-      <div className="w-full flex-between gap-5 py-4">
-        {/* <div className="home__text-container">
+      <div className="w-full gap-5 py-4">
+        <div className="home__text-container mb-8">
           <h1 className="text-4xl font-extrabold">Ulasan Pelanggan</h1>
           <p className="text-grey">Apa kata mereka tentang Honda</p>
-        </div> */}
+        </div>
 
         {/* Grid Layout Carousel */}
         <ReviewCarousel reviews={allReviews} gridLayout={true} />
